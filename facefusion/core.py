0a311840bdf04adbcf605e0fca7bc303a92fe531
tangqy <121102723@qq.com>
2024-10-27 22:34:06 +0800

    update image many to many swap support

--- facefusion/core.py
+++ facefusion/core.py
@@ -199,7 +199,7 @@ def apply_args(program : ArgumentParser,arg_list) -> None:
 	facefusion.globals.output_image_quality = args.output_image_quality
 	if "s3" in facefusion.globals.target_path :
 		pre_download()
-	if is_image(args.target_path):
+	if is_image(facefusion.globals.target_path):
 		output_image_resolution = detect_image_resolution(args.target_path)
 		output_image_resolutions = create_image_resolutions(output_image_resolution)
 		if args.output_image_resolution in output_image_resolutions:
